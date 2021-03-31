fdadd2ec134ed311540d35780e2e423ba0ae9d6b
Mark Griffin <magriffin@chapman.edu>
2021-03-31 00:02:58 -0700

    Finish BackgroundSelection template, added continue button functionality

--- Assets/Scripts/CharacterCreation Scripts/CharacterCreation.cs
+++ Assets/Scripts/CharacterCreation Scripts/CharacterCreation.cs
@@ -5,9 +5,18 @@ using UnityEngine.UI;
 
 public class CharacterCreation : MonoBehaviour
 {
+    // For testing
     public Text PetSelected;
+
     public Text PetName;
     public Text UserName;
+
+    // For testing
+    public Text PetNameInputted;
+    public Text UserNameInputted;
+
+    // For testing
+    public Text BackgroundSelected;
 
     private string petName;
     private string userName;
@@ -16,17 +25,23 @@ public class CharacterCreation : MonoBehaviour
     public GameObject BackgroundPrefab; // Maybe could just be an image?
 
     // Potential Canvas groups, buttons, text, etc.
+    public List<CanvasGroup> PageCanvasGroups;
+    private int totalPages;
+    private int currentPage;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        totalPages = PageCanvasGroups.Count;
+        currentPage = 1;
+
+        UpdatePages();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        UpdateInputtedTextFields();
     }
 
     // Will be the overall method that handles all of the functions relating to the player choosing their pet, giving it a name, ect.
@@ -70,4 +85,62 @@ public class CharacterCreation : MonoBehaviour
         PetSelected.text = "Pet Selected: Pet3";
     }
 
+    public void OnBackground1Press()
+    {
+        BackgroundSelected.text = "Pet Background Selected: Background1";
+    }
+
+    public void OnBackground2Press()
+    {
+        BackgroundSelected.text = "Pet Background Selected: Background2";
+    }
+
+    public void OnBackground3Press()
+    {
+        BackgroundSelected.text = "Pet Background Selected: Background3";
+    }
+
+    // For testing
+    private void UpdateInputtedTextFields()
+    {
+        SetPetName();
+        SetUserName();
+        PetNameInputted.text = "PetName Inputted: " + GetPetName();
+        UserNameInputted.text = "UserName Inputted: " + GetUserName();
+    }
+
+    public void OnContinuePress()
+    {
+        currentPage++;
+        if (currentPage > totalPages)
+        {
+            currentPage = totalPages;
+        }
+
+        UpdatePages();
+    }
+
+    private void UpdatePages()
+    {
+        foreach (CanvasGroup canvasGroup in PageCanvasGroups)
+        {
+            Hide(canvasGroup);
+        }
+
+        Show(PageCanvasGroups[currentPage - 1]);
+    }
+
+    private void Show(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 1;
+        canvasGroup.blocksRaycasts = true;
+        canvasGroup.interactable = true;
+    }
+
+    private void Hide(CanvasGroup canvasGroup)
+    {
+        canvasGroup.alpha = 0;
+        canvasGroup.blocksRaycasts = false;
+        canvasGroup.interactable = false;
+    }
 }
